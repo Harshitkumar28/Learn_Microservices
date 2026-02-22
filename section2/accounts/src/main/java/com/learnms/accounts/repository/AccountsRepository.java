cc13e3a8d47dd3887b7930bbfb63d218d71421b9
Harshitkumar28 <91775153+Harshitkumar28@users.noreply.github.com>
2026-02-23 00:52:02 +0530

    first commit

---
+++ section2/accounts/src/main/java/com/learnms/accounts/repository/AccountsRepository.java
@@ -0,0 +1,9 @@
+package com.learnms.accounts.repository;
+
+import com.learnms.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+}
