4bde9c42f9991498a92221aec2729ad487c4ffb5
Harshitkumar28 <91775153+Harshitkumar28@users.noreply.github.com>
2026-04-18 10:47:08 +0530

    Create the  CREATE API inside accounts microservice

--- section2/accounts/src/main/java/com/learnms/accounts/controller/AccountsController.java
+++ section2/accounts/src/main/java/com/learnms/accounts/controller/AccountsController.java
@@ -1,13 +1,33 @@
 package com.learnms.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.learnms.accounts.constants.AccountsConstants;
+import com.learnms.accounts.dto.CustomerDto;
+import com.learnms.accounts.dto.ResponseDto;
+import com.learnms.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountsController {
 
-    @GetMapping("sayHello")
-    public String sayHello(){
-        return "Hello World";
+    private IAccountsService iAccountsService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+
+
+        iAccountsService.createAccount(customerDto);
+
+        ResponseDto response =  new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+
     }
 }
