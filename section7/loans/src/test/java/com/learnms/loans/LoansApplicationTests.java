13592795be7190a69923e5ca19d53ddc01a265bb
Harshitkumar28 <91775153+Harshitkumar28@users.noreply.github.com>
2026-06-21 13:54:37 +0530

    feat: added mysql as docker container for all three services

---
+++ section7/loans/src/test/java/com/learnms/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnms.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
